d928830ee1d325fc03df3ed5a1811d322b3c13e3
min-kyoung <shalsrud9@naver.com>
2022-04-04 20:35:57 +0900

    Initial Commit

---
+++ Translate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Translate
+//
+//  Created by 노민경 on 2022/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
